1d15dae268464ca04d195a7cbe4ffecfd9a0ca4f
gabeschoenbach <gabe.schoenbach@gmail.com>
2023-09-12 13:49:05 -0500

    add near snappable stuff

--- heuristics.py
+++ heuristics.py
@@ -5,13 +5,32 @@ from tqdm import tqdm
 import click
 import os
 
+def make_biclique(n):
+    """
+    Generates a complete bipartite graph (biclique) where one part of the graph
+    has size 2n and the other has size 4n.
+    """
+    g = nx.Graph()
+    small_side = range(1, 2 * n + 1)
+    big_side = range(2 * n + 1, 6 * n + 1)
+    g.add_nodes_from(small_side, bipartite = 0)
+    g.add_nodes_from(big_side, bipartite = 1)
+    edges = [(s, b) for s in small_side for b in big_side]
+    g.add_edges_from(edges)
+    
+    assert nx.is_bipartite(g)
+    return g
+    
+
 def generate_graph(size, kind):
     if kind == "square":
         graph = nx.grid_graph(dim=(size,size))
-    elif kind == "complete":
+    elif kind == "clique":
         graph = nx.complete_graph(size)
+    elif kind == "biclique":
+        graph = make_biclique(size)
     else:
-        raise Exception("Can only accept 'square' or 'complete' graphs.")
+        raise Exception("Can only accept 'square', 'clique', or 'biclique' graphs.")
     return graph
     
 def count_snaps(trials, size, kind):
@@ -36,7 +55,7 @@ def run_experiment(kind, trials):
     with open(f"heuristics/{kind}_{trials}.csv", "a") as f:
         f.write("size,proportion_snaps\n")
     
-    for size in [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]:
+    for size in [2, 4, 8, 16]:
         proportion = count_snaps(trials, size, kind)
         with open(f"heuristics/{kind}_{trials}.csv", "a") as f:
             f.write(f"{size},{proportion}\n")
